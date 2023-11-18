085468e8308e97c1900643936d0030a06dedfc9c
ThatGuyGilby <jack.m.gilbride@gmail.com>
2023-11-18 15:01:28 +0000

    Example plugin

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace LethalCompanyPlugin
+{
+    [BepInPlugin("org.bepinex.plugins.lethalcompanyexample", "Example Plug-In", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
